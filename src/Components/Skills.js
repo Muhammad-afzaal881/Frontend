1c05181ee4d930390c701331af4a7f81a3543cd9
abbas-tr <abbas.a@turing.com>
2026-02-21 10:38:42 +0500

    updated

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,7 +1,8 @@
 import "./Skills.css";
-// Added SiNodejs, SiMongodb, SiExpress, SiFirebase for backend
-import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaGitAlt } from "react-icons/fa";
-import { SiNodejs, SiMongodb, SiExpress, SiFirebase, SiNextdotjs } from "react-icons/si";
+// FaNodeJs ko yahan add karein
+import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaGitAlt, FaBootstrap, FaNodeJs } from "react-icons/fa";
+// SiNodejs ko yahan se hata dein
+import { SiMongodb, SiExpress} from "react-icons/si";
 
 function Skills() {
   const skillsData = [
@@ -9,7 +10,7 @@ function Skills() {
     { id: 2, name: "CSS", icon: <FaCss3Alt />, color: "#1572B6" },
     { id: 3, name: "JavaScript", icon: <FaJs />, color: "#F7DF1E" },
     { id: 4, name: "React JS", icon: <FaReact />, color: "#61DAFB" },
-    { id: 5, name: "Node.js", icon: <SiNodejs />, color: "#339933" }, // Added
+    { id: 5, name: "Node.js", icon: <FaNodeJs />, color: "#339933" },
     { id: 6, name: "Express JS", icon: <SiExpress />, color: "#000000" }, // Added
     { id: 7, name: "MongoDB", icon: <SiMongodb />, color: "#47A248" }, // Added
     { id: 10, name: "Git", icon: <FaGitAlt />, color: "#F05032" },
