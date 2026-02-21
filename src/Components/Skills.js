f8c7154bafa615046c2cc6d2beb5f8e49013a966
abbas-tr <abbas.a@turing.com>
2026-02-21 10:35:33 +0500

    add skills

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,44 +1,19 @@
 import "./Skills.css";
-import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaGitAlt, FaEllipsisH } from "react-icons/fa";
+// Added SiNodejs, SiMongodb, SiExpress, SiFirebase for backend
+import { FaHtml5, FaCss3Alt, FaJs, FaReact, FaGitAlt } from "react-icons/fa";
+import { SiNodejs, SiMongodb, SiExpress, SiFirebase, SiNextdotjs } from "react-icons/si";
 
 function Skills() {
   const skillsData = [
-    {
-      id: 1,
-      name: "HTML",
-      icon: <FaHtml5 />,
-      color: "#E34F26",
-    },
-    {
-      id: 2,
-      name: "CSS",
-      icon: <FaCss3Alt />,
-      color: "#1572B6",
-    },
-    {
-      id: 3,
-      name: "JavaScript",
-      icon: <FaJs />,
-      color: "#F7DF1E",
-    },
-    {
-      id: 4,
-      name: "React JS",
-      icon: <FaReact />,
-      color: "#61DAFB",
-    },
-    {
-      id: 5,
-      name: "Git",
-      icon: <FaGitAlt />,
-      color: "#F05032",
-    },
-    {
-      id: 6,
-      name: "See More",
-      icon: <FaEllipsisH />,
-      color: "#38BDF8",
-    },
+    { id: 1, name: "HTML", icon: <FaHtml5 />, color: "#E34F26" },
+    { id: 2, name: "CSS", icon: <FaCss3Alt />, color: "#1572B6" },
+    { id: 3, name: "JavaScript", icon: <FaJs />, color: "#F7DF1E" },
+    { id: 4, name: "React JS", icon: <FaReact />, color: "#61DAFB" },
+    { id: 5, name: "Node.js", icon: <SiNodejs />, color: "#339933" }, // Added
+    { id: 6, name: "Express JS", icon: <SiExpress />, color: "#000000" }, // Added
+    { id: 7, name: "MongoDB", icon: <SiMongodb />, color: "#47A248" }, // Added
+    { id: 10, name: "Git", icon: <FaGitAlt />, color: "#F05032" },
+    { id: 11, name: "Bootstrap", icon: <FaBootstrap />, color: "#7952B3" },
   ];
 
   return (
